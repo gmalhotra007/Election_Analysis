d8d1fbcc01c04bac581c7f928cb9c1c33f045e73
Gurnoor Malhotra <gurnoormalhotra@icloud.com>
2020-11-07 23:51:53 -0500

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,11 @@
+#counties = ["Arapahoe","Denver","Jefferson"]
+
+#for i in range(len(counties)):
+    #print(counties[i])
+#numbers = [0,1,2,3,4,5]
+
+#for num in range(5):
+    #print(num)
+counties_dict = {"Arapahoe":422829, "Denver":463353,"Jefferson":432438}
+for x in counties_dict.keys():
+    print(x)
